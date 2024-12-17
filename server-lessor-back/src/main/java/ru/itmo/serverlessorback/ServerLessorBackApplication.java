49538905fa8934130e1781bc04cae7ed42dbf176
Аскаров Эмиль Рамилевич <askarov.emii@yandex.ru>
2024-12-17 21:08:21 +0300

    feat: add tariff

--- server-lessor-back/src/main/java/ru/itmo/serverlessorback/ServerLessorBackApplication.java
+++ server-lessor-back/src/main/java/ru/itmo/serverlessorback/ServerLessorBackApplication.java
@@ -1,5 +1,6 @@
 package ru.itmo.serverlessorback;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -7,7 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories
 public class ServerLessorBackApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ServerLessorBackApplication.class, args);
     }
---
+++ server-lessor-back/src/main/java/ru/itmo/serverlessorback/controller/model/response/JwtResponse.java
@@ -0,0 +1,8 @@
+package ru.itmo.serverlessorback.controller.model.response;
+
+import lombok.Data;
+
+@Data
+class JwtResponse {
+    private String accessToken;
+}
